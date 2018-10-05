fb19bd9bda250648eaf08801cadffb0bec6f491a
Eric Andres <emandres@gmail.com>
2018-10-05 14:52:09 -0400

    Clean up - remove unused using and move FizzBuzz class to its own file

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string Say(int n)
+        {
+            string result = string.Empty;
+            if (n % 3 == 0)
+            {
+                result += "Fizz";
+            }
+            if (n % 5 == 0)
+            {
+                result += "Buzz";
+            }
+
+            if (result != string.Empty)
+            {
+                return result;
+            }
+
+            return n.ToString();
+        }
+    }
+}
--- FizzBuzz/FizzBuzzTests.cs
+++ FizzBuzz/FizzBuzzTests.cs
@@ -46,27 +46,4 @@ namespace FizzBuzz
             Assert.That(FizzBuzz.Say(n), Is.EqualTo(n.ToString()));
         }
     }
-
-    public class FizzBuzz
-    {
-        public static string Say(int n)
-        {
-            string result = string.Empty;
-            if (n % 3 == 0)
-            {
-                result += "Fizz";
-            }
-            if (n % 5 == 0)
-            {
-                result += "Buzz";
-            }
-
-            if (result != string.Empty)
-            {
-                return result;
-            }
-
-            return n.ToString();
-        }
-    }
 }
